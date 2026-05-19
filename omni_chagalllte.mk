75d614f814f708e3f63a62a0a4a2eb6923b9943c
MrGadget84 <superhyper52@gmail.com>
2026-05-19 10:49:09 +0400

    Update omni_chagalllte.mk

--- omni_chagalllte.mk
+++ omni_chagalllte.mk
@@ -2,10 +2,7 @@
 PRODUCT_RELEASE_NAME := chagalllte
 
 $(call inherit-product, vendor/fox/config/tw_generic.mk)
-$(call inherit-product, vendor/fox/config/dirac.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := chagalllte
 PRODUCT_MODEL := SM-T805
 PRODUCT_NAME := omni_chagalllte
 PRODUCT_BRAND := samsung
