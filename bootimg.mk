0a7ea0b357154b7b37ec2ab45f2534c6e6b5593f
MrGadget84 <superhyper52@gmail.com>
2026-05-19 09:28:41 +0400

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -1,18 +1,14 @@
 LOCAL_PATH := $(call my-dir)
-
 RECOVERY_TAR := $(PRODUCT_OUT)/recovery.tar
-
-$(recovery_ramdisk): $(recovery_uncompressed_ramdisk)
+$(recovery_ramdisk): $(recovery_uncompressed_ramdisk_target)
 	$(call pretty,"Target LZMA ramdisk: $@")
-	$(hide) xz --format=lzma --lzma1=dict=16MiB -9 -c < $(recovery_uncompressed_ramdisk) > $@
-
+	$(hide) xz --format=lzma --lzma1=dict=16MiB -9 -c < $(recovery_uncompressed_ramdisk_target) > $@
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_kernel) $(recovery_ramdisk)
-	@echo -e ${CL_GRN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(recovery_ramdisk)
-	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
-	@echo -e ${CL_GRN}"----- Lying about SEAndroid state to Samsung bootloader ------"${CL_RST}
-	$(hide) echo -n "SEANDROIDENFORCE" >> $(INSTALLED_RECOVERYIMAGE_TARGET)
+	@echo -e "----- Making recovery image -----"
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	@echo -e "Made recovery image: $@"
+	@echo -e "----- Adding SEAndroid enforce tag for Samsung bootloader -----"
+	$(hide) echo -n "SEANDROIDENFORCE" >> $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(RECOVERY_TAR)
-	@echo -e ${CL_CYN}"Made Odin flashable recovery tar: $(RECOVERY_TAR)"${CL_RST}
-
+	@echo -e "Made Odin flashable recovery tar: $(RECOVERY_TAR)"
