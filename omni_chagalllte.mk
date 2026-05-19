c65d96b82475a982b7abfe03182955e9ec23a648
MrGadget84 <superhyper52@gmail.com>
2026-05-19 10:20:06 +0400

    Update omni_chagalllte.mk

--- omni_chagalllte.mk
+++ omni_chagalllte.mk
@@ -1,7 +1,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := chagalllte
 
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/fox/config/tw_generic.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := chagalllte
