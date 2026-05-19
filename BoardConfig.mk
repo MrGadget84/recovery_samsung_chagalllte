4cb04450a4c842b688784a32fcf05cc6ede8ec51
MrGadget84 <superhyper52@gmail.com>
2026-05-19 09:24:36 +0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,11 +8,6 @@ TARGET_BOOTLOADER_BOARD_NAME := universal5420
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_BOARD_PLATFORM_GPU := mali-t628mp6
 
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DNO_SECURE_DISCARD
-
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -20,32 +15,33 @@ TARGET_CPU_VARIANT := cortex-a15
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_IS_64_BIT := false
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DNO_SECURE_DISCARD
 
 # Kernel
-#TARGET_KERNEL_SOURCE := kernel/samsung/universal5420
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_CONFIG := twrp_defconfig
-TARGET_KERNEL_DEVICE_DEFCONFIG := device_chagalllte_can
-
 TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/zImage
 
 # Boot image
-BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/bootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/bootimg.mk
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x000800000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x000A00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x095600000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x2EF7FC000 # 0x2EF800000 - 16384 (crypto footer)
-BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x00C800000
-BOARD_HIDDENIMAGE_PARTITION_SIZE   := 0x00FA00000
-BOARD_FLASH_BLOCK_SIZE := 0x20000
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760   # Это всего 10 МБ! Критически мало.
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2506096640
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12608323584
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 209715200
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Stub dynamic partitions flag to prevent compilation abortion in TWRP 11+
+BOARD_SUPER_PARTITION_GROUPS :=
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -53,32 +49,35 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# TWRP specific build flags
-TW_THEME := landscape_hdpi
+# TWRP & OrangeFox Display Settings
+TW_THEME := portrait_hdpi
+BOARD_HAS_FLIPPED_SCREEN := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+# Hardware Paths
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/exynos-dwc3.0/exynos-ss-udc.0/gadget/lun%d/file"
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/s5p-mipi-dsim.1/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 162
+
+# Samsung Specific Flags
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_NTFS_3G := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 
-# exFAT drivers included in the kernel
+# File Systems drivers
+TW_INCLUDE_NTFS_3G := true
 TW_NO_EXFAT_FUSE := true
 
-# No love for the wicked (very small partition)
+# Exclude heavy components to fit into 10MB recovery partition
 TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_CRYPTO := false
+TWRP_INCLUDE_LOGCAT := false
+TW_EXCLUDE_APEX := true
 
-# Encryption support
-TW_INCLUDE_CRYPTO := true
+# OrangeFox Optimization Flags for Ultra-Low Storage Devices
+DISABLE_RECOVERY_PROMO := true
+FOX_EMPTY_INITD := true
+TW_OUTPUT_STRIP := true
 
-# Debug flags
-TWRP_INCLUDE_LOGCAT := true
-
-# Init properties from bootloader version, ex. model info
-TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_chagalllte
-TARGET_RECOVERY_DEVICE_MODULES := libinit_chagalllte
-TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_chagalllte.cpp
