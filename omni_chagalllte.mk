849cb59de223669350a1882d0fbf689ebdea25e3
MrGadget84 <superhyper52@gmail.com>
2026-05-20 17:50:35 +0400

    Update omni_chagalllte.mk

--- omni_chagalllte.mk
+++ omni_chagalllte.mk
@@ -3,7 +3,9 @@ PRODUCT_RELEASE_NAME := chagalllte
 
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_MODEL := SM-T805
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := chagalllte
 PRODUCT_NAME := omni_chagalllte
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := galaxy
+PRODUCT_MODEL := galaxy tab
 PRODUCT_MANUFACTURER := samsung
